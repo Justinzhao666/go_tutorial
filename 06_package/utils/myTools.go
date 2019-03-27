c0af8d66d4d701f0d7199de3b45f4e17812818b9
zhaohaoren <haoren.zhao@innotree.cn>
2019-03-27 22:26:01 +0800

    v100-120

---
+++ 06_package/utils/myTools.go
@@ -0,0 +1,13 @@
+// 包是作为一个类的，而包中的文件名是没有什么特殊用处的，对外使用包内方法的时候不需要走文件名，而是走包名
+
+package utils
+
+import "fmt"
+
+func PublicFunction() {
+	fmt.Println("this is a public function")
+}
+
+func privateFunction() {
+	fmt.Println("this is a private function")
+}
---
+++ 06_package/utils/myTools2.go
@@ -0,0 +1,11 @@
+package utils
+
+func Main()  {
+	privateFunction()
+}
+
+
+// 同一个包下不可以重复定义变量和函数。
+//func PublicFunction()  {
+//
+//}
