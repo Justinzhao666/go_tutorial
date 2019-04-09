322da80b7942e0c29e3f63c714bdb811dd1e9fff
zhaohaoren <haoren.zhao@innotree.cn>
2019-04-09 22:45:47 +0800

    v250-255

--- 41_file.go
+++ 41_file.go
@@ -14,7 +14,7 @@ import (
 // go操作文件使用os.File，其中File是一个结构体
 
 func main() {
-
+/*
 	//#1 打开文件
 	// open 会返回一个file对象，和一个错误信息
 	// file 还可以叫file指针，文件句柄等
@@ -42,16 +42,20 @@ func main() {
 		fmt.Println("read all error:", e)
 	}
 	fmt.Printf("%v", string(bytes))
-
+*/
 	//#3 关闭文件
 
 	//e = file.Close()
 	//if e != nil {
 	//	fmt.Println("file close error:", e)
 	//}
+	//
+	//writeFile()
+	//
+	//copyFile()
+	fmt.Println(copyFile2())
 
-	writeFile()
-
+	//fmt.Println(PathExist("./41_file"))
 }
 
 func writeFile() {
@@ -66,4 +70,58 @@ func writeFile() {
 	writer := bufio.NewWriter(file) // 带缓存的写入
 	writer.WriteString(str)         // 这一步其实是先将文件写入缓存的
 	writer.Flush()                  // 将缓存刷新到文件中
+}
+
+// 使用ioutil来完成文件的拷贝
+func copyFile() {
+	file1 := "./41_file"
+	file2 := "./41_file_copy"
+
+	data, e := ioutil.ReadFile(file1)
+	if e != nil {
+		fmt.Println("read file error:", e)
+	}
+	err := ioutil.WriteFile(file2, data, 0666)
+	if err != nil {
+		fmt.Println("write file error", err)
+	}
+}
+
+// 使用流进行读写
+func copyFile2() (written int64, err error) {
+	file1 := "./41_file"
+	file2 := "./41_file_copy.txt"
+
+	srcFile, e := os.Open(file1)
+	if e != nil {
+		fmt.Println(e)
+	}
+	defer srcFile.Close()
+	//reader := bufio.NewReader(srcFile)
+
+	destFile, err := os.OpenFile(file2, os.O_WRONLY | os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//writer := bufio.NewWriter(destFile)
+	defer destFile.Close()
+
+	//return io.Copy(writer, reader)  //这种方法不可行-拷贝文件没内容！(视频讲错了，这copy最后还是需要flush刷入文件中)
+	return io.Copy(destFile, srcFile) // 可以直接使用这样拷贝
+}
+
+// 判断文件是否存在
+func PathExist(path string) (bool, error) {
+	_, e := os.Stat(path)
+	if e == nil {
+		// 文件存在无异常
+		return true, nil
+	}
+	// 异常为文件不存在返回
+	if os.IsNotExist(e) {
+		return false, nil
+	}
+	// 判断出现异常，无法判断是否存在文件返回
+	return false, e
 }
